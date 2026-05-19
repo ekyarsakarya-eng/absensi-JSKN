aa4278e8408064bdf17760bbd9833b4b84c75cb8
ekyarsakarya-eng <eky.arsakarya@gmail.com>
2026-05-19 10:00:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-const CACHE_NAME = 'jaskin-v4';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('absensi-v1').then(c => c.addAll(['/', '/index.html', '/app.js'])));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.map(k => {
-        if (k !== CACHE_NAME) return caches.delete(k);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
