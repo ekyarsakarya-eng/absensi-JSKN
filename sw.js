1d244b21de37c2d75780ae8b871a9c1ffa79bc39
Eko Didie Yuliantho <eky.arsakarya@gmail.com>
2026-06-14 19:10:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = 'absensi-jskn-v4';
+const CACHE_NAME = 'absensi-jskn-v7';
 const urlsToCache = [
   './',
   './index.html',
   './app.js',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 self.addEventListener('install', event => {
